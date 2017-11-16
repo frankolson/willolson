e3c1003eb20ed5c15d212cd562c0a3b681a6e3af
Will Olson <will.olson11@gmail.com>
2017-11-16 14:11:10 -0800

    feat: Add and setup styled components

--- static.config.js
+++ static.config.js
@@ -1,3 +1,7 @@
+// Vendor Assets
+import React, { Component } from 'react'
+import { ServerStyleSheet } from 'styled-components'
+
 // Project Assets
 import * as api from './utils/api'
 
@@ -36,5 +40,27 @@ export default {
         component: 'src/containers/404',
       },
     ]
+  },
+  renderToHtml: (render, Comp, meta) => {
+    const sheet = new ServerStyleSheet()
+    const html = render(sheet.collectStyles(<Comp />))
+    meta.styleTags = sheet.getStyleElement()
+    return html
+  },
+  Document: class CustomHtml extends Component {
+    render () {
+      const { Html, Head, Body, children, renderMeta } = this.props
+
+      return (
+        <Html>
+          <Head>
+            <meta charSet="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            {renderMeta.styleTags}
+          </Head>
+          <Body>{children}</Body>
+        </Html>
+      )
+    }
   },
 }
