e64d8f1de42ae1f7a0b802b797e2b12fe347d91a
Will Olson <will.olson11@gmail.com>
2017-11-16 19:26:13 -0800

    feat: Update blog page

--- src/containers/Blog.js
+++ src/containers/Blog.js
@@ -1,21 +1,33 @@
 // Vendor Assets
 import React from 'react'
 import { getRouteProps } from 'react-static'
+import styled from 'styled-components'
 
 // Project Assets
-import Link from '../elements/Link'
+import Card from '../elements/Card'
+import { white } from '../../utils/colors'
+
+const BlogHeader = styled.div`
+  color: ${white};
+  text-align: center;
+`
+
+const Post = styled(Card)`
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  padding: 2rem;
+`
 
 export default getRouteProps(({ posts }) => (
-  <div>
-    <h1>It's blog time.</h1>
-    <br />
-    All Posts:
-    <ul>
-      {posts.map(post => (
-        <li key={post.slug}>
-          <Link to={`/blog/post/${post.slug}/`}>{post.body}</Link>
-        </li>
-      ))}
-    </ul>
+  <div className="container">
+    <BlogHeader>
+      <h1>It's blog time.</h1>
+    </BlogHeader>
+
+    {posts.map(post => (
+      <Post key={post.slug}>
+        {post.body}
+      </Post>
+    ))}
   </div>
 ))
