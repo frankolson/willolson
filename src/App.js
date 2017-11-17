ad2eaaa33aa44f5a433b226ddb7a9081af75ec9a
Will Olson <will.olson11@gmail.com>
2017-11-16 21:06:02 -0800

    feat: Adding styles for code tag

--- src/App.js
+++ src/App.js
@@ -18,6 +18,14 @@ injectGlobal`
     padding: 0;
     background: ${primaryDark};
   }
+
+  code {
+    padding: 0.2em 0.4em;
+    margin: 0;
+    font-size: 85%;
+    background-color: rgba(27,31,35,0.05);
+    border-radius: 3px;
+  }
 `
 
 const AppStyles = styled.div`
