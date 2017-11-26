8c4599bcc8c817915494c5e2f2efbfdc312f7374
Will Olson <will.olson11@gmail.com>
2017-11-25 17:45:16 -0800

    feat: Update home page text and remove react-static image

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,38 +4,28 @@ import { getSiteProps } from 'react-static'
 import styled from 'styled-components'
 
 // Project Assets
-import Card from '../elements/Card'
 import Container from '../elements/Container'
 import ProfilePicture from '../elements/ProfilePicture'
-import logoImg from '../logo.png'
 import { white } from '../../utils/colors'
 
-const HomeHeader = styled.h1`
-  color: ${white};
+const HomeContainer = styled(Container)`
+  max-width: 900px;
 `
 
-const HomeDescription = styled.p`
-  font-size: 24px;
+const HomeHeader = styled.p`
+  font-size: 2rem;
   color: ${white};
   margin-bottom: 2rem;
 `
 
 export default getSiteProps(() => (
-  <Container>
+  <HomeContainer>
     <ProfilePicture />
 
-    <HomeHeader>Hi, I'm Will</HomeHeader>
-
-    <HomeDescription>
-      I'm Full Stack Developer with a passion to improve the world on a grand
-      scale through technological advancements. I have helped multiple start-ups
-      and companies build out their products using Ruby on Rails and React. While
-      my current expertise is in web development, I have a degree in electrical
-      engineering and am interested in meshing those two interests together.
-    </HomeDescription>
-
-    <Card>
-      <img src={logoImg} alt="" />
-    </Card>
-  </Container>
+    <HomeHeader>
+      {/* eslint-disable max-len */}
+      Hi, I'm <b>Will Olson</b>, a Full Stack Developer with a passion to improve the world on a grand scale. I have helped multiple start-ups and companies build out their products using Ruby on Rails and React.
+      {/* eslint-enable max-len */}
+    </HomeHeader>
+  </HomeContainer>
 ))
