e50dc59640859af832f99f274a30229ef9c96fe4
Will Olson <will.olson11@gmail.com>
2017-12-11 15:55:49 -0800

    feat: Change date color to inherit

--- src/elements/DateRange.jsx
+++ src/elements/DateRange.jsx
@@ -1,19 +1,13 @@
 // Vendor Assets
 import React from 'react'
-import styled from 'styled-components'
 import moment from 'moment'
-
-const DateRange = styled.div`
-  color: grey;
-  font-style: italic;
-`
 
 const formatDate = date => (
   moment(date).format('MMM YYYY')
 )
 
 export default ({ endDate, present, startDate }) => (
-  <DateRange>
+  <div>
     {`${formatDate(startDate)} - ${present ? 'Present' : formatDate(endDate)}`}
-  </DateRange>
+  </div>
 )
