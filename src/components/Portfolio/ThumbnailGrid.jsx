cac4bba8709c0eaeaa5ec15c42ad516239b60462
Will Olson <will.olson11@gmail.com>
2017-12-11 20:46:35 -0800

    feat: Add portfolio grid

---
+++ src/components/Portfolio/ThumbnailGrid.jsx
@@ -0,0 +1,42 @@
+// Vendor Assets
+import React from 'react'
+import styled from 'styled-components'
+
+const Grid = styled.div`
+  @media screen and (min-width: 600px) {
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+  }
+`
+
+const Image = styled.img`
+  max-width: 100%;
+`
+
+const Cell = styled.div`
+  margin: 1rem;
+
+  img {
+    display: block;
+  }
+
+  @media screen and (min-width: 600px) {
+    width: calc(50% - 2rem);
+  }
+
+  @media screen and (min-width: 1000px) {
+    width: calc(33.3333% - 2rem);
+  }
+}
+`
+
+export default ({ items }) => (
+  <Grid>
+    {items.map(item => (
+      <Cell key={item.id}>
+        <Image src={item.thumbnail} alt={item.title} />
+      </Cell>
+    ))}
+  </Grid>
+)
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -6,6 +6,7 @@ import styled from 'styled-components'
 // Project Assets
 import Container from '../elements/Container'
 import ProfilePicture from '../elements/ProfilePicture'
+import PortfolioGrid from '../components/Portfolio/ThumbnailGrid'
 import { white } from '../../utils/colors'
 
 const HomeHeader = styled.p`
@@ -13,6 +14,18 @@ const HomeHeader = styled.p`
   color: ${white};
   margin-bottom: 2rem;
 `
+
+const PortfolioHeader = styled.div`
+  color: ${white};
+  text-align: center;
+`
+
+const placeholderData = [
+  { id: 1, thumbnail: 'http://placehold.it/500x500', title: 'project1' },
+  { id: 2, thumbnail: 'http://placehold.it/500x500', title: 'project2' },
+  { id: 3, thumbnail: 'http://placehold.it/500x500', title: 'project3' },
+  { id: 4, thumbnail: 'http://placehold.it/500x500', title: 'project4' },
+]
 
 export default getSiteProps(() => (
   <Container>
@@ -23,5 +36,13 @@ export default getSiteProps(() => (
       Hi, I'm <b>Will Olson</b>, a Full Stack Developer with a passion to improve the world on a grand scale. I have helped multiple start-ups and companies build out their products using Ruby on Rails and React.
       {/* eslint-enable max-len */}
     </HomeHeader>
+
+    <div>
+      <PortfolioHeader>
+        <h2>Check out some of my projects:</h2>
+      </PortfolioHeader>
+
+      <PortfolioGrid items={placeholderData} />
+    </div>
   </Container>
 ))
