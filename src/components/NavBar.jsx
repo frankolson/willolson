8fee4127bcd90696ef5de146d9cd19034c2064e3
Will Olson <will.olson11@gmail.com>
2017-12-14 14:14:13 -0800

    feat: Add a fixed transitioning header

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,12 +6,38 @@ import styled from 'styled-components'
 import Navlink from '../elements/Navlink'
 
 const Nav = styled.nav`
+  background-color: ${props => props.background};
+  position: fixed;
+  top: 0;
+  transition: background-color 0.5s;
   width: 100%;
 `
 
-export default () => (
-  <Nav>
-    <Navlink to="/" exact>Home</Navlink>
-    <Navlink to="/resume">Resume</Navlink>
-  </Nav>
-)
+export default class NavBar extends React.Component {
+  state = {
+    background: null,
+  }
+
+  componentDidMount = () => {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  componentWillUnmount = () => {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = () => {
+    this.setState({
+      background: window.pageYOffset === 0 ? null : 'rgba(0,0,0,0.2)',
+    })
+  }
+
+  render () {
+    return (
+      <Nav background={this.state.background}>
+        <Navlink to="/" exact>Home</Navlink>
+        <Navlink to="/resume">Resume</Navlink>
+      </Nav>
+    )
+  }
+}
